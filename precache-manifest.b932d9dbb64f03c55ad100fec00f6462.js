150b4192b2ce65e093c841bb2d81119c905f04b2
bella-cockrell <heybellac@gmail.com>
2019-12-08 16:34:31 +0000

    Updates

---
+++ precache-manifest.b932d9dbb64f03c55ad100fec00f6462.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "117810df1c99dbceadb1d0735c88af8f",
+    "url": "/my-favourite-flowers-app/index.html"
+  },
+  {
+    "revision": "b5979f089f6aeaa20384",
+    "url": "/my-favourite-flowers-app/static/css/main.60f72c90.chunk.css"
+  },
+  {
+    "revision": "80233528b79727fd4f45",
+    "url": "/my-favourite-flowers-app/static/js/2.1805597b.chunk.js"
+  },
+  {
+    "revision": "b5979f089f6aeaa20384",
+    "url": "/my-favourite-flowers-app/static/js/main.4b703220.chunk.js"
+  },
+  {
+    "revision": "259dacce04d824595e63",
+    "url": "/my-favourite-flowers-app/static/js/runtime-main.c2ae0e3b.js"
+  },
+  {
+    "revision": "82d9965a77361cc210c18eff21e138aa",
+    "url": "/my-favourite-flowers-app/static/media/dahlia.82d9965a.jpg"
+  },
+  {
+    "revision": "b5bc17fcbcb6baf549011b3c3080fcb7",
+    "url": "/my-favourite-flowers-app/static/media/peony.b5bc17fc.jpg"
+  }
+]);
